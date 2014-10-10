c6481ef8fcc9891c9d5171e37c7d1366c984b23e
Kevin Klues <klueska@cs.berkeley.edu>
2014-10-10 15:17:19 -0700

    Update test to more accurately show what we want
    This test never terminates still, so I'll need to write one that does
    for benchmarking, but you cna launhc a bunch of these guys in parallel
    in different tabs and watch them all fight for cores to run all their
    threads.  Pretty cool stuff.

--- tests/preempt_test.c
+++ tests/preempt_test.c
@@ -5,22 +5,20 @@
 #include <assert.h>
 
 /* OS dependent #incs */
+#include <sys/time.h>
 #include <parlib/parlib.h>
 #include <parlib/vcore.h>
-#include <sys/time.h>
+#include <parlib/timing.h>
 
-#define udelay(usec) usleep(usec)
-
-upthread_mutex_t lock = UPTHREAD_MUTEX_INITIALIZER;
-//#define printf_safe(...) {}
-#define printf_safe(...) \
-	upthread_mutex_lock(&lock); \
+#define printd(...) \
+{ \
+	upthread_disable_interrupts(); \
 	printf(__VA_ARGS__); \
-	upthread_mutex_unlock(&lock);
+	upthread_enable_interrupts(); \
+}
 
 #define MAX_NR_TEST_THREADS 100000
-int nr_threads = 100;
-int nr_loops = 100;
+int nr_threads = 1000;
 int nr_vcores = 0;
 int amt_fake_work = 0;
 
@@ -31,31 +29,38 @@ bool ready = FALSE;
 
 void *worker_thread(void* arg)
 {	
+	int id = (int)(long)arg;
+	long long nr_loops = 0;
 	/* Wait til all threads are created */
+	printd("Entered thread: %d, vcore: %d\n", id, vcore_id());
 	while (!ready)
 		cpu_relax();
-	//for (int i = 0; i < nr_loops; i++) {
-	printf("Entered thread: %d\n", upthread_self()->id);
-	while(1);
+
+	while(1) {
+		if ((nr_loops++ % 1000000000LL) == 0)
+			printd("Looping on thread: %d, vcore: %d, c: %lld\n", id, vcore_id(), nr_loops);
+	}
 	return NULL;
 }
 
 int main(int argc, char** argv) 
 {
+	//upthread_set_sched_period(1000000);
 	/* OS dependent prep work */
 	if (nr_vcores) {
 		/* Only do the vcore trickery if requested */
 		upthread_can_vcore_request(FALSE);	/* 2LS won't manage vcores */
 		vcore_request(nr_vcores - 1);		/* ghetto incremental interface */
 		for (int i = 0; i < nr_vcores; i++) {
-			printf("Vcore %d not mapped to a particular pcore\n", i);
+			printd("Vcore %d not mapped to a particular pcore\n", i);
 		}
 	}
 
 	/* Create Threads */
 	for (int i = 0; i < nr_threads; i++) {
-		printf_safe("[A] About to create thread %d\n", i);
-		assert(!upthread_create(&my_threads[i], NULL, &worker_thread, NULL));
+		printd("[A] About to create thread %d\n", i);
+		assert(!upthread_create(&my_threads[i], NULL,
+		                        &worker_thread, (void*)(long)i));
 	}
 	ready = TRUE;			/* signal to any spinning uthreads to start */
 	while(1);
