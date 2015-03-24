e5884f2a6891eef5b78d3aa6b9b65c2306c909dc
Kevin Klues <klueska@cs.berkeley.edu>
2015-03-24 09:38:10 -0700

    Print function name on error, not just filename

--- src/common/unsupported.c
+++ src/common/unsupported.c
@@ -4,26 +4,30 @@ int pthread_mutex_timedlock (pthread_mutex_t *__restrict __mutex,
 				    const struct timespec *__restrict
 				    __abstime)
 {
-	perror("Unsupported " __FILE__ "!");
+	fprintf(stderr, "Unsupported %s!", __FUNCTION__);
+	abort();
 	return -1;
 }
 int pthread_cond_timedwait (pthread_cond_t *__restrict __cond,
 				   pthread_mutex_t *__restrict __mutex,
 				   const struct timespec *__restrict __abstime)
 {
-	perror("Unsupported " __FILE__ "!");
+	fprintf(stderr, "Unsupported %s!", __FUNCTION__);
+	abort();
 	return -1;
 }
 
 int upthread_once (upthread_once_t *__once_control,
                    void (*__init_routine) (void))
 {
-	perror("Unsupported " __FILE__ "!");
+	fprintf(stderr, "Unsupported %s!", __FUNCTION__);
+	abort();
 	return -1;
 }
 
-int pthread_cancel (pthread_t __th)
+int upthread_cancel (pthread_t __th)
 {
-	perror("Unsupported " __FILE__ "!");
+	fprintf(stderr, "Unsupported %s!", __FUNCTION__);
+	abort();
 	return -1;
 }
